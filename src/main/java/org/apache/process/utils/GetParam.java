e5717e2e68102795fbcb8175c38afc677927fcb3
Wuyunfan-BUPT <1564396093@qq.com>
2023-06-10 13:10:22 +0800

    add parseParams

--- src/main/java/org/apache/process/utils/GetParam.java
+++ src/main/java/org/apache/process/utils/GetParam.java
@@ -21,8 +21,12 @@ package org.apache.process.utils;
 
 import org.apache.commons.cli.CommandLine;
 import org.apache.commons.cli.Option;
+import org.json.JSONObject;
+import org.yaml.snakeyaml.Yaml;
 
 import java.util.HashMap;
+import java.util.Map;
+import java.util.TreeMap;
 
 public class GetParam {
     public HashMap<String, String> setParam(CommandLine cmd) {
@@ -32,5 +36,30 @@ public class GetParam {
             result.put(option.getLongOpt(), option.getValue());
         }
         return result;
+    }
+
+    public static HashMap<String, String> parseParams(String s, String target){
+        StringBuilder builder = new StringBuilder();
+        StringBuilder targetBuilder = new StringBuilder();
+        String[] lines = s.split("\n");
+        boolean isTargetText = false;
+        for(String line:lines){
+            if(line.startsWith(target)){
+                isTargetText = true;
+                continue;
+            }
+            if(isTargetText && line.startsWith("  ")){
+                targetBuilder.append(line).append("\n");
+            }else{
+                builder.append("  ").append(line).append("\n");
+                isTargetText = false;
+            }
+        }
+        Yaml yaml = new Yaml();
+        HashMap<String,String> builderMap= (HashMap<String,String>) yaml.load(builder.toString());
+        HashMap<String,String> targetMap= (HashMap<String, String>) yaml.load(targetBuilder.toString());
+        JSONObject jsonObject=new JSONObject(targetMap);
+        builderMap.put(target, jsonObject.toString().replaceAll("\"", "\\\\\"").toString());
+        return builderMap;
     }
 }
---
+++ src/test/java/org/apache/process/utils/GetParamTest.java
@@ -0,0 +1,69 @@
+package org.apache.process.utils;
+
+import junit.framework.Assert;
+import org.apache.process.model.Deploymodel;
+import org.junit.Test;
+
+import java.util.HashMap;
+
+import static org.apache.process.utils.GetParam.parseParams;
+
+public class GetParamTest {
+    @Test
+    public void testParseParams() throws Exception{
+        String helmvalues =
+                "global:\n" +
+                        "  mode: standalone\n" +
+                        "nacos:\n" +
+                        "  replicaCount: 1\n" +
+                        "  image: \n" +
+                        "    repository: wuyfeedocker/nacos-ci\n" +
+                        "    tag: develop-4f26def4-ccb0-45e5-9989-874e78424bea-8\n" +
+                        "  storage:\n" +
+                        "    type: embedded\n" +
+                        "    db:\n" +
+                        "      port: 3306\n" +
+                        "      username: nacos\n" +
+                        "      password: nacos\n" +
+                        "      param: characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useSSL=false\n" +
+                        "service:\n" +
+                        "  nodePort: 30009\n" +
+                        "  type: ClusterIP";
+        String params = "username: nacos\n" +
+                "password: hello\n" +
+                "cmd: mvn -B install\n"+
+                "helm:\n" +
+                "  chart: java/e2e\n"+
+                "  git:\n"+
+                "    branch: master\n"+
+                "  repoType: git\n"+
+                "  retries: 3\n"+
+                "  url: https://ghproxy.com/https://github.com/apache/rocketmq-e2e.git\n"+
+                "  values:\n" +
+                "    global:\n" +
+                "      mode: standalone\n" +
+                "    nacos:\n" +
+                "      replicaCount: 1\n" +
+                "      image: \n" +
+                "        repository: wuyfeedocker/nacos-ci\n" +
+                "        tag: develop-4f26def4-ccb0-45e5-9989-874e78424bea-8\n" +
+                "      storage:\n" +
+                "        type: embedded\n" +
+                "        db:\n" +
+                "          port: 3306\n" +
+                "          username: nacos\n" +
+                "          password: nacos\n" +
+                "          param: characterEncoding=utf8&connectTimeout=1000&socketTimeout=3000&autoReconnect=true&useSSL=false\n" +
+                "    service:\n" +
+                "      nodePort: 30009\n" +
+                "      type: ClusterIP";
+        String target = "helm:";
+        HashMap<String, String> mapp = parseParams(params, target);
+        for(String key: mapp.keySet()){
+            System.out.println(key+": "+ mapp.get(key));
+        }
+        Assert.assertEquals(mapp.get("username"), "nacos");
+        Assert.assertEquals(Deploymodel.generateComponentProperties(helmvalues,"java/e2e","master", "https://ghproxy.com/https://github.com/apache/rocketmq-e2e.git").length(), mapp.get("helm:").length());
+        System.out.println(mapp.get("helm:"));
+    }
+}
