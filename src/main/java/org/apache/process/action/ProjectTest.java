b92ca8d43158819bafe59c252e47db1ba1646a7c
Wuyunfan-BUPT <1564396093@qq.com>
2023-06-11 19:02:29 +0800

    add parseParams

---
+++ src/main/java/org/apache/process/action/ProjectTest.java
@@ -0,0 +1,111 @@
+/*
+ * #
+ * # Licensed to the Apache Software Foundation (ASF) under one or more
+ * # contributor license agreements.  See the NOTICE file distributed with
+ * # this work for additional information regarding copyright ownership.
+ * # The ASF licenses this file to You under the Apache License, Version 2.0
+ * # (the "License"); you may not use this file except in compliance with
+ * # the License.  You may obtain a copy of the License at
+ * #
+ * #     http://www.apache.org/licenses/LICENSE-2.0
+ * #
+ * # Unless required by applicable law or agreed to in writing, software
+ * # distributed under the License is distributed on an "AS IS" BASIS,
+ * # WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * # See the License for the specific language governing permissions and
+ * # limitations under the License.
+ * #
+ */
+
+package org.apache.process.action;
+
+import io.kubernetes.client.custom.Quantity;
+import io.kubernetes.client.openapi.ApiException;
+import io.kubernetes.client.openapi.apis.CoreV1Api;
+import io.kubernetes.client.openapi.models.*;
+
+import java.io.IOException;
+import java.util.HashMap;
+import java.util.LinkedHashMap;
+import java.util.Random;
+
+import static org.apache.process.utils.GetParam.yamlToMap;
+
+public class ProjectTest {
+
+    public boolean runTest(String namespace, String input) throws ApiException, IOException, InterruptedException {
+        System.out.println("**************E2E TEST...***************");
+        CoreV1Api api = new CoreV1Api();
+
+        System.out.println("**************create pod***************");
+        /* create pod */
+        LinkedHashMap<String, Object> inputMap = yamlToMap(input);
+        String testPodName = "test-" + inputMap.get("env") + new Random().nextInt(100000);
+        // create a pod
+
+        V1Pod pod_template = new V1Pod();
+        pod_template.setApiVersion(inputMap.get("API_VERSION").toString());
+        pod_template.setKind(inputMap.get("KIND").toString());
+        // set pod: metadata
+        pod_template.setMetadata(new V1ObjectMeta().name(testPodName).namespace(namespace));//
+        // set pod: spec
+        V1PodSpec pod_template_spec = new V1PodSpec();
+        pod_template_spec.setRestartPolicy(inputMap.get("RESTART_POLICY").toString());
+        // set pod spec: container
+        V1Container container = new V1Container();
+        System.out.println("**********************" + testPodName + "****************");
+
+
+        // ***********set container***********
+        // set pod spec container: name and images
+        LinkedHashMap<String, Object>  containerMap = (LinkedHashMap)inputMap.get("CONTAINER");
+        container.setName(testPodName);
+        container.setImage(containerMap.get("IMAGE").toString());
+        // set pod spec container: resource
+        LinkedHashMap<String, Object>  limitsResourceMap = (LinkedHashMap)containerMap.get("RESOURCE_LIMITS");
+        HashMap<String, Quantity> resourcesLimits = new HashMap<>();
+        for(String limitKey: limitsResourceMap.keySet()){
+            resourcesLimits.put(limitKey, new Quantity(limitsResourceMap.get(limitKey).toString()));
+        }
+        LinkedHashMap<String, Object>  requestResourceMap = (LinkedHashMap)containerMap.get("RESOURCE_REQUIRE");
+        HashMap<String, Quantity> resourceRequests = new HashMap<>();
+        for(String requestKey: requestResourceMap.keySet()){
+            resourcesLimits.put(requestKey, new Quantity(requestResourceMap.get(requestKey).toString()));
+        }
+        V1ResourceRequirements v1ResourceRequirements = new V1ResourceRequirements();
+        v1ResourceRequirements.setLimits(resourcesLimits);
+        v1ResourceRequirements.setRequests(resourceRequests);
+        container.setResources(v1ResourceRequirements);
+
+        // set V container: env elements
+        LinkedHashMap<String, Object> envMap = (LinkedHashMap)inputMap.get("ENV");
+        if(envMap.get("ALL_IP")==null || "null".equals(envMap.get("ALL_IP"))){
+            /* get all IP */
+            V1PodList pods = api.listNamespacedPod(namespace, null, null, null, null, null, null, null, null, null, null);
+            StringBuilder allIP = new StringBuilder();
+            for (V1Pod pod : pods.getItems()) {
+                allIP.append(pod.getMetadata().getName()).append(":").append(pod.getStatus().getPodIP()).append(",");
+            }
+            envMap.put("ALL_IP", allIP.substring(0, allIP.length()-1));
+            System.out.println(allIP);
+        }
+
+        for(String envKey: envMap.keySet()){
+            V1EnvVar v1EnvVar = new V1EnvVar();
+            v1EnvVar.setName(envKey);
+            v1EnvVar.setValue(envMap.get(envKey).toString());
+            container.addEnvItem(v1EnvVar);
+        }
+
+        // add container to spec
+        pod_template_spec.addContainersItem(container);
+
+        // add spec to pod
+        pod_template.setSpec(pod_template_spec);
+
+        // create pod
+        V1Pod createdPod = api.createNamespacedPod(namespace, pod_template, null, null, null, null);
+        System.out.println(createdPod.getStatus().getPhase());
+        return new QueryTestPod().getPodResult(testPodName, namespace, envMap.get("CMD").toString(), envMap.get("CODE_PATH").toString());
+    }
+}
