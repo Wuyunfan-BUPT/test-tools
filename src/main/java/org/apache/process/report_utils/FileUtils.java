7cd71bee5dac18f294c75e09e227ee1d2a06bbbd
Wuyunfan-BUPT <1564396093@qq.com>
2023-06-20 20:44:46 +0800

    remove redundant code in FileUtils.java

--- src/main/java/org/apache/process/report_utils/FileUtils.java
+++ src/main/java/org/apache/process/report_utils/FileUtils.java
@@ -2,17 +2,9 @@ package org.apache.process.report_utils;
 
 import lombok.extern.slf4j.Slf4j;
 import org.apache.commons.codec.digest.DigestUtils;
-import org.apache.commons.lang3.StringUtils;
 import org.apache.commons.lang3.exception.ExceptionUtils;
 
 import java.io.*;
-import java.nio.file.FileVisitResult;
-import java.nio.file.Path;
-import java.nio.file.Paths;
-import java.nio.file.SimpleFileVisitor;
-import java.nio.file.attribute.BasicFileAttributes;
-import java.util.zip.ZipEntry;
-import java.util.zip.ZipOutputStream;
 
 /**
  * Created by wangtong.wt on 2017/3/20.
